1f7165c6a7d901ca9482c3451b69efb655ce0807
David Towster <djt4xe@virginia.edu>
2018-04-05 14:06:15 -0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
 
 function init() {
   // Write your JavaScript code inside the init() function
+  var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+  var counter = 0;
+
+  document.addEventListener('keydown', onKeyDownHandler);
+
+  function onKeyDownHandler(e) {
+    var key = parseInt(e.detail || e.which);
+    if (key === code[counter]) {
+      counter++;
+
+      if (counter === 10) {
+        alert('I was clicked!');
+        counter = 0;
+      } 
+    } else {
+      counter = 0;
+    }
+  }
 
 }
